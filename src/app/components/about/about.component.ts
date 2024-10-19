c54de4b6b5f5481e620141161923b6866c7afa67
Stanciu Mihai <misu_sergiu@yahoo.com>
2024-10-19 20:09:55 +0300

    fixed a router link not showing as clickable problem

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -4,7 +4,7 @@ import { Router,RouterLink,RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
